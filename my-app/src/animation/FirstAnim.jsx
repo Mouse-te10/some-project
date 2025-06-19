159a7fad94549be0f205842f1c9d075f9f363117
Bogdan Matmusaev <bogdanmatmusave@gmail.com>
2025-06-19 17:15:42 +0500

    animation

---
+++ my-app/src/animation/FirstAnim.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import './index.css'
+
+function FirstAnim() {
+    return (
+        <div className="first-animation">
+            <div className="slider slider-one"></div>
+
+            <div className="slider-top"></div>
+
+            <div className="slider slider-two"></div>
+        </div>
+    )
+}
+
+export default FirstAnim
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import FirstAnim from './animation/FirstAnim.jsx'
 import Nav from './navLinks.jsx'
 import Header from './header.jsx'
 import SectionOne from './sectionOne.jsx'
@@ -10,6 +11,7 @@ import './index.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <FirstAnim />
     <Nav />
     <Header />
     <SectionOne />
